76d0f1198432038f2451f0a0e94d86f17742cdae
ning <tianshan@me.com>
2018-07-28 20:18:46 +0800

    exec

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Mosca
+#
+# VERSION 2.5.2
+
+FROM mhart/alpine-node:4
+MAINTAINER Matteo Collina <hello@matteocollina.com>
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app/
+
+COPY ./ /usr/src/app/
+
+RUN apk update && \
+    apk add make gcc g++ python git zeromq-dev krb5-dev && \
+    npm install --unsafe-perm --production && \
+    apk del make gcc g++ python git
+
+EXPOSE 80
+
+ENTRYPOINT ["/usr/src/app/index.js"]
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const Promise = require('bluebird')
+#! /usr/bin/env node
+
+const child_process = require('child_process')
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
-
-const child_process = require('child_process')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.get('/snapshot.png', (req, res)=>{
-    const child = child_process.execFile('/home/pi/raspi2png/raspi2png', ['-c', '8'], (error, stdout, stderr) => {
+    const child = child_process.execFile(__dirname + '/raspi2png', ['-c', '8'], (error, stdout, stderr) => {
         if (error) {
             console.error(error, stderr);
             res.sendStatus(500)
@@ -21,5 +21,5 @@ app.get('/snapshot.png', (req, res)=>{
       });
 })
 
-const port = process.env.GET_SHOT_PORT || 8080
+const port = process.env.GET_SHOT_PORT || 80
 app.listen(port, ()=> console.log('Server Listen on ' + port))
